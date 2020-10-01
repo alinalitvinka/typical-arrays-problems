d823b97e4366b4772384cc359d483886f837c511
alinalitvinka <alinalitvinko7@gmail.com>
2020-10-01 10:33:31 +0300

    feat: add function min

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 
 exports.min = function min (array) {
-  
+  if (array === undefined || array.length === 0) {
   return 0;
+  } else {
+  return Math.min.apply(null, array);
+}
 }
 
 exports.max = function max (array) {
+  
   return 0;
 }
 
